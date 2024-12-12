55b660c6eb64e71bc333815be5dea692b9004a66
Fran <fsannicceb@educacion.navarra.es>
2024-12-12 03:05:33 +0100

    commit 2 casa

---
+++ Programación/IntelliJ proojects/AD01 Histograma AL/src/Histograma.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+/**
+ * La clase contiene un array que se inicializa 
+ * con una serie de valores y se utiliza para dibujar
+ * un histograma
+ */
+
+public class Histograma
+{
+    private final static int MAX = 10;
+    public final char ASTERISCO = '*';
+    private int[] valores ;
+    
+    /**
+     * Constructor
+     */
+    public Histograma()
+    {
+         
+
+    }
+
+    /**
+     * inicializa el array con valores aleatorios
+     */
+    private void inicializar()
+    {
+         
+    }
+
+    /**
+     * genera y devuelve un aleatorio entre 5 y 50 inclusive
+     */
+    private int generarAleatorio()
+    {
+         
+    }
+
+    /**
+     *  muestra el histograma
+     */
+    public void escribirHistograma()
+    {
+         
+    }
+
+ 
+    /**
+     * 
+     * devuelve una copia del array 
+     */
+    public int[] getValoresV1()
+    {
+        
+    }
+
+     /**
+     * 
+     * devuelve una copia del array 
+     */
+    public int[] getValoresV2()
+    {
+        
+    }
+       
+}
